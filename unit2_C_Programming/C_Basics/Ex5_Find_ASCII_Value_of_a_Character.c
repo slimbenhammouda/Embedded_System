a7f0f7352c7723ddb9156c3a81a78235754d4156
slim <slimbenhammouda5@gmail.com>
2023-11-27 19:06:44 +0100

    apdate

---
+++ unit2_C_Programming/C_Basics/Ex5_Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,22 @@
+/*
+ * ##############################################
+ * Ex5_Find_ASCII_Value_of_a_Character.c
+ *      project:Find ASCII Value of a Character.
+ *      Created on: Nov 27, 2023
+ *      Author:Slim benhammouda.
+ * ##############################################
+ */
+#include<stdio.h>
+int main()
+{
+	char ch ;
+	printf("Enter a character:");
+	fflush(stdin); fflush(stdout);
+	scanf("%c",&ch);
+	printf("ASCII value of %c = %d",ch,ch);
+	return 0 ;
+}
+
+
+
+
