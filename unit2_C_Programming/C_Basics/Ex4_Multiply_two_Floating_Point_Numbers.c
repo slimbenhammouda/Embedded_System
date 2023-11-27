6eaba3d278126313f0d061f0f84d245899c7be38
slim <slimbenhammouda5@gmail.com>
2023-11-27 18:28:00 +0100

    apdate

---
+++ unit2_C_Programming/C_Basics/Ex4_Multiply_two_Floating_Point_Numbers.c
@@ -0,0 +1,20 @@
+/*
+ * #######################################
+ * Multiply_two_Floating_Point_Numbers.c
+ *      project:Multiply two Floating Point Numbers
+ *      Created on: Nov 27, 2023
+ *      Author: slim benhammouda.
+ * #######################################
+ */
+#include<stdio.h>
+int main()
+{
+	float a,b ;
+	printf("Enter two numbers:");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f",&a,&b);
+	printf("sum:%f",a*b);
+	return 0 ;
+}
+
+
