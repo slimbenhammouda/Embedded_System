65c5960c9f1702e7d64ef5a6ed5a1def5ea90d62
slim <slimbenhammouda5@gmail.com>
2023-11-27 18:12:11 +0100

    apdate

---
+++ unit2_C_Programming/C_Basics/Ex3_Add_two_integers.c
@@ -0,0 +1,22 @@
+/*
+ * ################################
+ * Ex3_Add_two_integers.c
+ *      project :add two integers.
+ *      Created on: Nov 27, 2023
+ *      Author: slim benhammouda .
+ * ################################
+ */
+#include<stdio.h>
+int main()
+{
+	int a,b ;
+	printf("Enter two integers :");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d",&a,&b);
+	printf("sum:%d",a+b);
+	return 0 ;
+}
+
+
+
+
