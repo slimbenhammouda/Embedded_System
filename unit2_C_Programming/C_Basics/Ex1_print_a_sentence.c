6da984198a3cf66c52c7b9806f54a4463ead251d
slim <slimbenhammouda5@gmail.com>
2023-11-27 17:09:10 +0100

    apdate

---
+++ unit2_C_Programming/C_Basics/Ex1_print_a_sentence.c
@@ -0,0 +1,18 @@
+/*
+ * ######################################
+ *  main.c
+ *  project: print a sentence.
+ *  Created on: Nov 27, 2023.
+ *  Author:slim benhammouda.
+ *  #####################################
+ */
+#include<stdio.h>
+int main()
+{
+	printf("C programming");
+	return 0 ;
+}
+
+
+
+
