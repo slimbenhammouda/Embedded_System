c0eef70bfb78efaa2043da16a29e32a28f422bfb
slim <slimbenhammouda5@gmail.com>
2023-11-27 17:44:18 +0100

    apdat

---
+++ unit2_C_Programming/C_Basics/Ex2_Enter_An_integer.c
@@ -0,0 +1,22 @@
+/*
+ * ################################################
+ * Ex2_Enter_An_integer.c
+ *      project: print an integer entred by a user.
+ *      Created on: Nov 27, 2023
+ *      Author: R I B
+ * ################################################
+ */
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter an integer :");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&a);
+	printf("you entred: %d",a);
+	return 0 ;
+}
+
+
+
+
