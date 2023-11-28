4e060b530b896b3372c38cfcb65349810c582a86
slim <slimbenhammouda5@gmail.com>
2023-11-28 14:55:57 +0100

    apdate

---
+++ unit2_C_Programming/loops_&_condition/Ex2_check_whether_an_alphabet_is_vowel_or_consonant.c
@@ -0,0 +1,28 @@
+/*
+ * ##############################################################
+ * Ex2_check_whether_an_alphabet_is_vowel_or_consonant.c
+ *      project:check whether an alphabet is vowel or consonant.
+ *      Created on: Nov 28, 2023
+ *      Author:Slim benhammouda.
+ * ##############################################################
+ */
+#include<stdio.h>
+int main()
+{
+	char ch;
+	int vowel;
+	printf("Enter an alphabet:");
+	fflush(stdin); fflush(stdout);
+	scanf("%c",&ch);
+	vowel=(ch=='a'||ch=='e'||ch=='u'||ch=='i'||ch=='o'||ch=='A'||ch=='E'||ch=='U'||ch=='I'||ch=='O');
+
+	if(vowel)
+		printf("%c is a vowel",ch);
+	else
+		printf("%c is a consonant",ch);
+	return 0 ;
+}
+
+
+
+
