58a891f3dcf27140673258b2939f587611dbc642
slim <slimbenhammouda5@gmail.com>
2023-11-28 12:08:16 +0100

    apdate

---
+++ unit2_C_Programming/C_Basics/Ex7_Swap_Two_Numbers_without_temp_variable.c
@@ -0,0 +1,30 @@
+/*
+ * ####################################################
+ * Ex7_Swap_Two_Numbers_without_temp_variable.c
+ *      Project:Swap Two Numbers without temp variable.
+ *      Created on: Nov 28, 2023
+ *      Author:Slim benhammouda.
+ * ####################################################
+ */
+#include<stdio.h>
+int main()
+{
+	float num1,num2;
+	printf("enter value of a:");
+	fflush(stdin); fflush(stdout);
+	scanf("%f",&num1);
+	printf("enter value of b:");
+	fflush(stdin); fflush(stdout);
+	scanf("%f",&num2);
+	num1+=num2;
+	num2=num1-num2;
+	num1-=num2;
+	printf("After swapping, value of a =%f",num1);
+
+	printf("\nAfter swapping, value of b =%f",num2);
+	return 0 ;
+}
+
+
+
+
