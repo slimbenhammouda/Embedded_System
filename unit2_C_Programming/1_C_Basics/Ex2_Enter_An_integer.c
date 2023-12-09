b9eccf8224a720f5aa01f1c0d0e4142652ee252e
slim <150297948+slimbenhammouda@users.noreply.github.com>
2023-12-09 19:49:35 +0100

    Update Ex2_Enter_An_integer.c

--- unit2_C_Programming/1_C_Basics/Ex2_Enter_An_integer.c
+++ unit2_C_Programming/1_C_Basics/Ex2_Enter_An_integer.c
@@ -3,7 +3,7 @@
  * Ex2_Enter_An_integer.c
  *      project: print an integer entred by a user.
  *      Created on: Nov 27, 2023
- *      Author: slim benhammouda
+ *      Author: slim benhammouda 
  * ################################################
  */
 #include<stdio.h>
