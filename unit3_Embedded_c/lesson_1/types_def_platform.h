05901951fd3dcc841e55ce5f568b652cc1bc11b1
slim <slimbenhammouda5@gmail.com>
2024-02-25 22:53:36 +0100

    update

---
+++ unit3_Embedded_c/lesson_1/types_def_platform.h
@@ -0,0 +1,30 @@
+/*
+         ###############################
+
+    	    Author:Slim benhammouda
+
+         ###############################
+*/
+
+
+#ifndef TYPES_DEF_PLATFORM_H_
+#define TYPES_DEF_PLATFORM_H_
+
+typedef signed char int8_t ;
+typedef unsigned char   uint8_t;
+typedef short  int16_t;
+typedef unsigned short  uint16_t;
+typedef int  int32_t;
+typedef unsigned int  uint32_t;
+typedef long long  int64_t;
+typedef unsigned long long   uint64_t;
+typedef volatile int8_t vint8_t;
+typedef volatile uint8_t vuint8_t;
+typedef volatile int16_t vint16_t;
+typedef volatile uint16_t vuint16_t;
+typedef volatile int32_t vint32_t;
+typedef volatile uint32_t vuint32_t;
+typedef volatile int64_t vint64_t;
+typedef volatile uint64_t vuint64_t;
+
+#endif 
